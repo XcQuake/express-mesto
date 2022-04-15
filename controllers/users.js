0e68d57517c0b51a0aef983a9c03984d89a0ffb7
XcQuid <creed.35@yandex.ru>
2022-04-15 12:54:42 +0300

    feat: add card/user models, controllers and routes

---
+++ controllers/users.js
@@ -0,0 +1,39 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.getUser = (req, res) => {
+  User.find({ _id: req.params.userId })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  const profileId = req.user._id;
+
+  User.findByIdAndUpdate(profileId, { name, about }, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const profileId = req.user._id;
+
+  User.findByIdAndUpdate(profileId, { avatar }, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
