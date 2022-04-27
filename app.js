c4941dd6d672fdd30c205d08a2dac275aa5bc0cf
XcQuid <creed.35@yandex.ru>
2022-04-27 14:47:08 +0300

    feat: add rate limiter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -9,12 +10,18 @@ const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 const app = express();
 
+app.use(limiter);
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
