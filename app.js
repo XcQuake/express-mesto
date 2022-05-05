b84c5ab146866d9723c170114071f26967d5d459
XcQuid <creed.35@yandex.ru>
2022-05-05 11:46:23 +0300

    feat: add logger

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const { errorHandler } = require('./middlewares/errorHandler');
 const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -27,8 +28,9 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
 app.use(router);
-
+app.use(errorLogger);
 app.use(errors());
 
 app.use(errorHandler);
